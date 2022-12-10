38302940b785a9082a187e5e0a4b181f2cba7edc
sachin-borde <ssborde899@gmail.com>
2022-12-10 11:07:45 +0530

    swagger changes

--- src/main/java/com/hcl/SpringBootMongodbJpaApplication.java
+++ src/main/java/com/hcl/SpringBootMongodbJpaApplication.java
@@ -2,12 +2,41 @@ package com.hcl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
+@EnableSwagger2
+@EnableWebMvc
 public class SpringBootMongodbJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMongodbJpaApplication.class, args);
 	}
 
+	@Bean
+	public Docket docket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("employee-api")
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.hcl.controller"))
+				.paths(PathSelectors.any())
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Employee API")
+				.description("Spring Boot Employee Reference")
+				.licenseUrl("https://www.google.com")
+				.version("1.0")
+				.build();
+	}
 }
